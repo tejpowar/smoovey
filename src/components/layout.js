0c607385a0bfc0185afe0e8dc3be57027a37dcc9
Tej Powar <tejwant.powar@sky.uk>
2023-02-17 16:16:31 +0000

    building page

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,6 +29,9 @@ const Layout = ({ children }) => {
            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"
                  integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF"
                  crossOrigin="anonymous" />
+           <link rel="preconnect" href="https://fonts.googleapis.com" />
+           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+           <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&display=swap" rel="stylesheet" />
            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"
                    integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ"
                    crossOrigin="anonymous" />
@@ -42,16 +45,16 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        {/*<footer*/}
+        {/*  style={{*/}
+        {/*    marginTop: `var(--space-5)`,*/}
+        {/*    fontSize: `var(--font-sm)`,*/}
+        {/*  }}*/}
+        {/*>*/}
+        {/*  © {new Date().getFullYear()} &middot; Built with*/}
+        {/*  {` `}*/}
+        {/*  <a href="https://www.gatsbyjs.com">Gatsby</a>*/}
+        {/*</footer>*/}
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,20 @@ import '../styles/styles.css';
 const IndexPage = () => (
   <Layout>
     <div className="container">
-        <div className="messageArea">
-            <h1>Landlords</h1>
-        </div>
+        <div className="row">
+            <div className="col-4">
+                <div className="messageArea">
+                    <h3 className="messageBoldHeading">Landlords</h3>
+                    <h3 className="messageLightHeading">Stay <span className="messageBoldHeading">Compliant,</span> </h3>
+                    <h3 className="messageLightHeading">for good </h3>
+                    <p className="content">Manage your property certificates in one place and get notified when it’s time to renew. It’s easy, secure, and completely free.</p>
+                </div>
 
+            </div>
+            <div className="col-8">
+                2 of 3 (wider)
+            </div>
+        </div>
     </div>
 
   </Layout>
