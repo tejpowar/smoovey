5e5c38cbe556e8044c64a92de1cc92c404c971d8
Tej Powar <tejwant.powar@sky.uk>
2023-02-21 11:32:53 +0000

    sort layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,24 @@ const IndexPage = () => (
   <Layout>
     <div className="container">
         <div className="row">
-            <div className="col-4">
+            <div className="col-12 col-md-6">
                 <div className="messageArea">
-                    <h3 className="messageBoldHeading">Landlords</h3>
-                    <h3 className="messageLightHeading">Stay <span className="messageBoldHeading">Compliant,</span> </h3>
-                    <h3 className="messageLightHeading">for good </h3>
+                    <h1 className="messageBoldHeading">Landlords</h1>
+                    <h1 className="messageLightHeading">Stay <span className="messageBoldHeading">Compliant,</span> </h1>
+                    <h1 className="messageLightHeading">for good </h1>
                     <p className="contentBody">Manage your property certificates in one place and get notified when it’s time to renew. It’s easy, secure, and completely free.</p>
                     <button type="button" className="buttonLong">Get Started <span className="arrow"></span> </button>
                 </div>
 
             </div>
-            <div className="col-8">
-                2 of 3 (wider)
+            <div className="mt-5 col-12 col-md-6">
+                <div className="messageArea">
+                    <h1 className="messageBoldHeading">Landlords</h1>
+                    <h1 className="messageLightHeading">Stay <span className="messageBoldHeading">Compliant,</span> </h1>
+                    <h1 className="messageLightHeading">for good </h1>
+                    <p className="contentBody">Manage your property certificates in one place and get notified when it’s time to renew. It’s easy, secure, and completely free.</p>
+                    <button type="button" className="buttonLong">Get Started <span className="arrow"></span> </button>
+                </div>
             </div>
         </div>
     </div>
