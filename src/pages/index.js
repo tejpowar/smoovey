83652aa0c9cc06b6f9a7a9816c779f47eaf51cc7
Tej Powar <tejwant.powar@sky.uk>
2023-02-27 22:54:45 +0000

    add blue background and customer text

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,11 +25,13 @@ const IndexPage = () => (
                     <StaticImage src="../images/Illustrations/Illustration 1.svg" alt="House image" />
                 </div>
             </div>
-            <div className="waveShape">
+            <div className="waveShape col-12">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#060f3a" fill-opacity="1" d="M0,96L80,133.3C160,171,320,245,480,234.7C640,224,800,128,960,85.3C1120,43,1280,53,1360,58.7L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
             </div>
-            <div className="blueBackground">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#060f3a" fill-opacity="1" d="M0,64L0,96L1440,96L1440,320L0,320L0,320Z"></path></svg>
+            <div className="blueBackground col-12">
+                <p className="customerComment">"Using Smoovey has made life so much easier. <br/> I can focus on X instead of Y, it's amazing."</p>
+                <p className="customerName">Joe Bloggs</p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100"><path fill="#060f3a" fill-opacity="1" d="M0,64L0,96L1440,96L1440,320L0,320L0,320Z"></path></svg>
             </div>
         </div>
     </div>
