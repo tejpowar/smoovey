dbeeabbdd84dac096b7e5e388d7ac9bc904fda80
Tej Powar <tejwant.powar@sky.uk>
2023-10-13 11:04:54 +0100

    render empty

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,7 +15,6 @@ const Layout = ({ children }) => {
   return (
     <>
        <Helmet>
-           <link rel="preload" href="/src/styles/styles.css" as="style" />
            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"
                  integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF"
                  crossOrigin="anonymous" />
@@ -24,9 +23,6 @@ const Layout = ({ children }) => {
            <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&display=swap" rel="stylesheet" />
            <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400&display=swap" rel="stylesheet" />
            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap" rel="stylesheet" />
-           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"
-                   integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ"
-                   crossOrigin="anonymous" />
        </Helmet>
       <Navbar />
       <div className="container-fluid"
